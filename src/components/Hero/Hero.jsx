c76789b7ee1e987c2780ce325012e2126f9a4020
bigboypai <paaai@icloud.com>
2024-03-24 14:05:54 -0600

    final fixes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,21 +2,21 @@ import './Hero.scss'
 // import background from '/background.png'
 
 const Hero = () => {
-  return (
-    <div className='hero-container' id='inicio'>
-        <div className='hero-container__title'>
-            TE DAMOS LA BIENVENIDA
+    return (
+        <div className='hero-container' id='inicio'>
+            <div className='hero-container__title'>
+                TE DAMOS LA BIENVENIDA
+            </div>
+            <div className='hero-container__text'>
+                Somos responsables y apasionados productores y distribuidores de hortalizas y verduras al por mayor. Nos comprometemos a ofrecerte la más alta calidad y frescura en cada uno de nuestros productos, garantizando su inocuidad para un consumo seguro.
+            </div>
+            <a href='https://wa.me/50683019087' target='_blank' className='hero-container__cta'>
+                <button className='hero-container__cta-button'>
+                    MÁS INFO
+                </button>
+            </a>
         </div>
-        <div className='hero-container__text'>
-            Somos responsables y apasionados productores y distribuidores de hortalizas y verduras al por mayor. Nos comprometemos a ofrecerte la más alta calidad y frescura en cada uno de nuestros productos, garantizando su inocuidad para un consumo seguro.
-        </div>
-        <div className='hero-container__cta'>
-            <button className='hero-container__cta-button'>
-                MÁS INFO
-            </button>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Hero
