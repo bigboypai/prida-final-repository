6597d88468fb95709e78f9085c73ce38fc7c0ad5
Abraham Calvo <abraham.calvo@accenture.com>
2025-05-12 10:54:41 -0600

    Commited

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -5,8 +5,8 @@ const Info = () => {
     <div className='info-container' id='quienes-somos'>
         <div className='info-container__section'>
             <h2 className='info-container__section-heading'> QUIENES SOMOS </h2>
-            <p className='info-container__section-paragraph'> En AGROCOMERCIAL PRIDA, hemos estado comercializando nuestros vegetales durante más de 15 años en Supermercados, Restaurantes, Procesadoras de alimentos y distribuidores, asegurando la disponibilidad durante todo el año. </p>
-            <p className='info-container__section-paragraph'> La mayoría de nuestros productos son cultivados en nuestras propias fincas, donde aplicamos las mejores prácticas agrícolas los 365 días del año para garantizar la máxima calidad, seguridad alimentaria y frescura en cada uno de nuestros vegetales. </p>
+            <p style={{fontSize: '18px'}} className='info-container__section-paragraph'> En AGROCOMERCIAL PRIDA, hemos estado comercializando nuestros vegetales durante más de 15 años en Supermercados, Restaurantes, Procesadoras de alimentos y distribuidores, asegurando la disponibilidad durante todo el año. </p>
+            <p  style={{fontSize: '18px'}} className='info-container__section-paragraph'> La mayoría de nuestros productos son cultivados en nuestras propias fincas, donde aplicamos las mejores prácticas agrícolas los 365 días del año para garantizar la máxima calidad, seguridad alimentaria y frescura en cada uno de nuestros vegetales. </p>
         </div>
         <div className='info-container__section' id='productos'>
             <h2 className='info-container__section-heading'> PRODUCTOS </h2>
@@ -34,6 +34,26 @@ const Info = () => {
               <div className='info-container__section-product__item'>
                 <img src="https://i0.wp.com/semillasplantae.com/wp-content/uploads/2022/04/mail35.jpeg?fit=750%2C750&ssl=1" alt="apio" />
                 <p>Culantro</p>
+              </div>
+              <div className='info-container__section-product__item'>
+                <img src="https://blog.cambiagro.com/wp-content/uploads/2024/08/cover-guia-cultivo-zanahoria.jpg" alt="Zanahoria" />
+                <p>Zanahoria</p>
+              </div>
+              <div className='info-container__section-product__item'>
+                <img src="https://eos.com/wp-content/uploads/2023/10/tomato-growing-main.jpg.webp" alt="Tomate" />
+                <p>Tomate</p>
+              </div>
+              <div className='info-container__section-product__item'>
+                <img src="https://www.cucinare.tv/wp-content/uploads/2019/01/19-01-23-Hierbas.jpg" alt="Hierbas" />
+                <p>Hierbas</p>
+              </div>
+              <div className='info-container__section-product__item'>
+                <img src="https://regionnet.com.ar/wp-content/uploads/2022/04/rabanitos-opt.jpg" alt="Rabanito" />
+                <p>Rabanito</p>
+              </div>
+              <div className='info-container__section-product__item'>
+                <img src="https://www.conasi.eu/blog/wp-content/uploads/2023/07/recetas-con-espinacas-1.jpg" alt="Espinaca" />
+                <p>Espinaca</p>
               </div>
             </div>
         </div>
--- src/components/Info2/Info2.jsx
+++ src/components/Info2/Info2.jsx
@@ -5,12 +5,12 @@ const Info = () => {
     <div className='info-container-bg'>
       <div className='info-container__section' id='mision-vision'>
         <h2 className='info-container__section-heading'> MISIÓN Y VISIÓN </h2>
-        <p className='info-container__section-paragraph'> En nuestra empresa, nos comprometemos a ofrecer solidez y confianza a cada uno de nuestros clientes mediante la provisión constante de productos de alta calidad y seguridad alimentaria a lo largo de todo el año. </p>
-        <p className='info-container__section-paragraph'> Nuestra visión es ser una empresa agrícola productiva e innovadora que expande su línea de productos más allá de las fronteras, con el objetivo de ofrecer calidad y frescura inigualables a nuestros clientes.. </p>
+        <p style={{fontSize: '18px'}}  className='info-container__section-paragraph'> En nuestra empresa, nos comprometemos a ofrecer solidez y confianza a cada uno de nuestros clientes mediante la provisión constante de productos de alta calidad y seguridad alimentaria a lo largo de todo el año. </p>
+        <p style={{fontSize: '18px'}}  className='info-container__section-paragraph'> Nuestra visión es ser una empresa agrícola productiva e innovadora que expande su línea de productos más allá de las fronteras, con el objetivo de ofrecer calidad y frescura inigualables a nuestros clientes.. </p>
       </div>
       <div className='info-container__section'>
         <h2 className='info-container__section-heading' id='contacto'> CONTACTO </h2>
-        <p className='info-container__section-paragraph'> Tienes un negocio y te gustaría que seamos tu proveedor? </p>
+        <p style={{fontSize: '18px'}}  className='info-container__section-paragraph'> Tienes un negocio y te gustaría que seamos tu proveedor? </p>
         <a href='https://wa.me/50683019087' target='_blank'>
           <button className='icsb'> MÁS INFO </button>
         </a>
